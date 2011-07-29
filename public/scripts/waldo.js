203605b4a531777abb6cfb79c495d5f8a5500711
Court Ewing <court@epixa.com>
2011-07-29 16:43:04 -0400

    Added functionality to change url with history.replaceState

--- public/scripts/waldo.js
+++ public/scripts/waldo.js
@@ -1,8 +1,27 @@
 (function(){
+    var defaultTitle = document.title;
     var form = document.getElementById('identity_form');
     var field = document.getElementById('screen_name');
     var twitter = new waldo.Twitter();
     var lastIdentity = null;
+
+    /**
+     * Changes the current state in browser history if the browser supports it
+     * 
+     * @param uri   REQUIRED: The uri to change the browser url to
+     * @param data  OPTIONAL: The data to pass along as the state object
+     * @param title OPTIONAL: The title to pass along as the state title
+     */
+    function changeState(uri, data, title)
+    {
+        if (!uri) {
+            throw 'No uri specified';
+        }
+
+        if (typeof window.history.replaceState == 'function') {
+            window.history.replaceState(data || {}, title || defaultTitle, uri);
+        }
+    }
 
     /**
      * Loads the users that follow the given twitter user
@@ -11,6 +30,8 @@
      */
     function loadUsers(name){
         if (!name) {
+            changeState('/');
+            lastIdentity = null;
             return false;
         }
 
@@ -18,7 +39,11 @@
             return false;
         }
 
+        // TODO: clear the current "results"
+
         lastIdentity = name;
+
+        changeState('?name=' + name, {name: name}, 'Waldo query for ' + name);
 
         twitter.request('followers/ids.json?screen_name=' + name, {}, 'GET', function(client){
             var ids = json_parse(client.responseText);
@@ -73,6 +98,12 @@
         var query = window.location.search.substr(1);
         var variables = query.split('&');
         var x;
+
+        window.onpopstate = function(event) {
+            if (event.state && event.state.name) {
+                loadUsers(event.state.name);
+            }
+        };
 
         form.onsubmit = function() {
             return loadUsers(field.value);
